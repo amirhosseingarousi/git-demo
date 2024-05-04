628e687b9f132827e31a1744443c421f0113b788
Amir Hossein Garousi <amirgarousi73@gmail.com>
2024-05-04 14:44:02 +0330

    create constructor for customer

--- src/com/tosan/eg/Customer.java
+++ src/com/tosan/eg/Customer.java
@@ -3,5 +3,9 @@ package com.tosan.eg;
 public class Customer {
     private Integer id;
     private String name;
-    
+
+    public Customer(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
