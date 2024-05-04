34c304b95caa67b8113aa1ea912a95d9389205f0
Amir Hossein Garousi <amirgarousi73@gmail.com>
2024-05-04 14:00:58 +0330

    add new text

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Git is awesome:)");
-        System.out.println("TEST");
+
+        System.out.println("Fresh start");
     }
 }
