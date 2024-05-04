16e3ad6616d9a8cc94493da801c4b6be4c2e9a24
Amir Hossein Garousi <amirgarousi73@gmail.com>
2024-05-04 11:26:38 +0330

    add id attr to person

--- src/com/tosan/eg/Person.java
+++ src/com/tosan/eg/Person.java
@@ -1,4 +1,13 @@
 package com.tosan.eg;
 
 public class Person {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
