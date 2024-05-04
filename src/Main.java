0cea771d0316d4ffd97d2fa3694ee55d805f1bf1
amirhosseingarousi <61373568+amirhosseingarousi@users.noreply.github.com>
2024-05-04 15:00:05 +0330

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Fresh start");
+        System.out.println("Fresh start");
+        System.out.println("Fresh start");
     }
 }
