80d532baec7f690bca204182aff0db018d168121
Amir Hossein Garousi <amirgarousi73@gmail.com>
2024-05-04 14:52:22 +0330

    create customer instance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import com.tosan.eg.Customer;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Fresh start");
+        Customer customer = new Customer(10, "Garousi");
     }
 }
